5c5dece6e6eb92162c3e741829d10b9f546c9847
Ruslan Shashkov <ruslan.shashkov@ibitcy.com>
2017-02-23 18:59:39 +0200

    Build

--- index.d.ts
+++ index.d.ts
@@ -2,17 +2,19 @@ import * as React from 'react';
 export declare abstract class StoreComponent<Props, State, StoreState> extends React.Component<Props, State> {
     stores: StoreState;
     private isStoreMounted;
-    abstract storeComponentDidMount(): void;
-    abstract storeComponentWillUnmount(): void;
-    abstract storeComponentWillReceiveProps(nextProps: Props): void;
-    abstract storeComponentWillUpdate(nextProps: Props, nextState: State): void;
-    abstract storeComponentDidUpdate(prevProps: Props, prevState: State): void;
+    storeComponentDidMount(): void;
+    storeComponentWillUnmount(): void;
+    storeComponentWillReceiveProps(nextProps: Props): void;
+    storeComponentWillUpdate(nextProps: Props, nextState: State): void;
+    storeComponentDidUpdate(prevProps: Props, prevState: State): void;
+    shouldStoreComponentUpdate(nextProps: Props, nextState: State): boolean;
     constructor(stores: StoreState);
     componentDidMount(): void;
     componentWillUnmount(): void;
     componentWillReceiveProps(nextProps: Props): void;
     componentWillUpdate(nextProps: Props, nextState: State): void;
     componentDidUpdate(prevProps: Props, prevState: State): void;
+    shouldComponentUpdate(nextProps: Props, nextState: State): void;
 }
 export declare class Store<StoreState> {
     components: any[];
