293cbca2559ab89b46bbb24f60736ccad32bf3f3
esa <egorovsa2@gmail.com>
2017-12-16 23:33:53 +0200

    fix IE Object assign

--- test/test.js
+++ test/test.js
@@ -3482,6 +3482,14 @@ var __extends = (this && this.__extends) || (function () {
         d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
     };
 })();
+var __assign = (this && this.__assign) || Object.assign || function(t) {
+    for (var s, i = 1, n = arguments.length; i < n; i++) {
+        s = arguments[i];
+        for (var p in s) if (Object.prototype.hasOwnProperty.call(s, p))
+            t[p] = s[p];
+    }
+    return t;
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 var React = __webpack_require__(6);
 var StoreComponent = /** @class */ (function (_super) {
@@ -3548,7 +3556,7 @@ var Store = /** @class */ (function () {
         this.initialState = this.copyState(state);
     }
     Store.prototype.copyState = function (state) {
-        return Object.assign({}, state);
+        return __assign({}, state);
     };
     Store.prototype.isCircular = function (obj) {
         try {
