b6ce45b6bda1eaedfa4360dee8651e7d80f6d7c1
Ruslan Shashkov <ruslanchek@gmail.com>
2018-02-26 19:18:25 +0200

    Fix exports

--- index.js
+++ index.js
@@ -2,3 +2,5 @@ const ReactStores = require('./lib/index');
 
 module.exports.StoreComponent = ReactStores.StoreComponent;
 module.exports.Store = ReactStores.Store;
+module.exports.followStore = ReactStores.followStore;
+module.exports.StoreEvent = ReactStores.StoreEvent;
