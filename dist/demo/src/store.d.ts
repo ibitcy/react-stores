82732a53ff077d5b69fc928f0757542ac67c8742
Ruslan Shashkov <ruslanchek@icloud.com>
2018-08-20 21:28:51 +0300

    Remove dist

--- dist/demo/src/store.d.ts
+++
@@ -1,8 +0,0 @@
-import { Store } from "../../src/store";
-export interface StoreState {
-    counter: number;
-    foo: string;
-}
-export declare const store: Store<StoreState>;
-export declare const storePersistent: Store<StoreState>;
-export declare const storeHistory: Store<StoreState>;
