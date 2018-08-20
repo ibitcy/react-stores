ea27b9ff5c50949e916bee9a84bfe374f09433a9
Ruslan Shashkov <ruslanchek@icloud.com>
2018-08-20 21:26:31 +0300

    update

--- dist/demo/src/counter-decorator.d.ts
+++
@@ -1,9 +0,0 @@
-import * as React from 'react';
-interface Props {
-}
-interface State {
-}
-export declare class CounterDecorator extends React.Component<Props, State> {
-    render(): JSX.Element;
-}
-export {};
