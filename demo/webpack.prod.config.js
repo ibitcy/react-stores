23fdeb0a4217dd68d326ce649c5e81164bce8cc7
Ruslan Shashkov <ruslanchek@icloud.com>
2018-08-20 21:41:56 +0300

    Refactor

---
+++ demo/webpack.prod.config.js
@@ -0,0 +1,56 @@
+const path = require('path');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+
+module.exports = {
+	entry: './demo/src/app.tsx',
+	output: {
+		filename: 'bundle.js',
+		path: path.join(__dirname, '../dist/demo'),
+	},
+
+	devtool: 'none',
+
+	resolve: {
+		extensions: ['.ts', '.tsx', '.js', '.json']
+	},
+
+	module: {
+		rules: [
+			{
+				test: /\.tsx?$/,
+				loader: 'awesome-typescript-loader',
+				options: {
+					types: false,
+				}
+			},
+			{
+				enforce: 'pre',
+				test: /\.js$/,
+				loader: 'source-map-loader'
+			},
+			{
+				test: /\.html$/,
+				use: [ {
+					loader: 'html-loader',
+					options: {
+						minimize: true,
+						removeComments: false,
+						collapseWhitespace: false
+					}
+				}]
+			}
+		]
+	},
+
+	plugins: [
+		new CopyWebpackPlugin([
+			{
+				from: path.join(__dirname, 'src/index.html'),
+				to: 'index.html'
+			}
+		], {
+			debug: false,
+			copyUnmodified: false
+		}),
+	]
+};
