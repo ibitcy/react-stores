be52c62b3648db614ef1b754a5fc263a4c21393f
Ruslan Shashkov <rshashkov@icloud.com>
2019-04-23 19:11:09 +0300

    Build

--- lib/use_store.d.ts
+++ lib/use_store.d.ts
@@ -1,6 +1,6 @@
-import { Store, StoreEventType } from "./store";
+import { Store, StoreEventType } from './store';
 export interface IUseStoreOptions<StoreState, MappedState> {
     eventType?: StoreEventType | StoreEventType[];
     mapState?: (storeState: StoreState) => MappedState;
 }
-export declare function useStore<StoreState = {}, MappedState = StoreState>(store: Store<StoreState>, options?: IUseStoreOptions<StoreState, MappedState>): StoreState | MappedState;
+export declare function useStore<StoreState = {}, MappedState = StoreState>(store: Store<StoreState>, options?: IUseStoreOptions<StoreState, MappedState>): MappedState;
