143c3b76ca7101d68ae35f6ea62a2cf7d33bb9d9
Konstantin24121 <konstantin24121@gmail.com>
2019-05-15 19:15:52 +0500

    Fix typings

--- src/use_store.ts
+++ src/use_store.ts
@@ -1,17 +1,18 @@
 import * as React from 'react';
 
-import { Store, StoreEventType } from './store';
+import {Store, StoreEventType} from './store';
 
 export interface IUseStoreOptions<StoreState, MappedState> {
   eventType?: StoreEventType | StoreEventType[];
   mapState?: (storeState: StoreState) => MappedState;
+  deps?: any[];
 }
 
 export function useStore<StoreState = {}, MappedState = StoreState>(
   store: Store<StoreState>,
   options: IUseStoreOptions<StoreState, MappedState> = {},
 ): MappedState {
-  const deps = options.deps || []; 
+  const deps = options.deps || [];
   const mapState = React.useCallback(
     (stateStore: StoreState): MappedState => (options.mapState ? options.mapState(stateStore) : (stateStore as any)),
     deps,
