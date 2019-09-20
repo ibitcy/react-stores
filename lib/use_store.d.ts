96ab1f50da7de861706f14372e485527b5d03974
ruslanchek <rshashkov@icloud.com>
2019-09-20 14:18:46 +0300

    useStore hook no haw proper types in deps option

--- lib/use_store.d.ts
+++ lib/use_store.d.ts
@@ -3,6 +3,6 @@ import { StoreEventType } from './StoreEvent';
 export interface IUseStoreOptions<StoreState, MappedState> {
     eventType?: StoreEventType | StoreEventType[];
     mapState?: (storeState: StoreState) => MappedState;
-    deps?: any[];
+    deps?: Array<keyof StoreState>;
 }
 export declare function useStore<StoreState = {}, MappedState = StoreState>(store: Store<StoreState>, options?: IUseStoreOptions<StoreState, MappedState>): MappedState;
