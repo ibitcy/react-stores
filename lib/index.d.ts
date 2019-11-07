5f3469045520b9dbe0461ad909339d7006cbf86c
Ruslan Shashkov <ruslan.s@expertoption.com>
2019-11-07 13:04:32 +0200

    Yarn

--- lib/index.d.ts
+++
@@ -1,6 +0,0 @@
-export * from './followStore';
-export * from './Store';
-export * from './StorePersistentLocalStorageDriver';
-export * from './useStore';
-export { StoreEvent, StoreEventType } from './StoreEvent';
-export { StorePersistentDriver } from './StorePersistentDriver';
