72fbb2cdee14d2b14ac46fa7282ccda276755afb
Ruslan Shashkov <ruslan.s@expertoption.com>
2019-11-07 13:07:17 +0200

    Update build

---
+++ lib/Store.d.ts
@@ -0,0 +1,40 @@
+import { StorePersistentDriver } from './StorePersistentDriver';
+import { StoreEventType, StoreEvent } from './StoreEvent';
+export interface StoreOptions {
+    /**
+     * @deprecated since 3.x: use immutable flag instead
+     */
+    live?: boolean;
+    /**
+     * @deprecated since 3.x: use immutable flag instead
+     */
+    freezeInstances?: boolean;
+    immutable?: boolean;
+    persistence?: boolean;
+    setStateTimeout?: number;
+}
+export declare class Store<StoreState> {
+    readonly persistenceDriver?: StorePersistentDriver<StoreState>;
+    components: any[];
+    readonly id: string;
+    private eventManager;
+    private readonly initialState;
+    private frozenState;
+    private opts;
+    get state(): StoreState;
+    constructor(initialState: StoreState, options?: StoreOptions, persistenceDriver?: StorePersistentDriver<StoreState>);
+    deepFreeze(obj: any): any;
+    private hashCode;
+    private generateStoreId;
+    resetPersistence(): void;
+    resetDumpHistory(): void;
+    saveDump(): number;
+    removeDump(timestamp: number): void;
+    restoreDump(timestamp: number): void;
+    getDumpHistory(): number[];
+    setState(newState: Partial<StoreState>): void;
+    resetState(): void;
+    update(currentState: StoreState, prevState: StoreState): void;
+    getInitialState(): StoreState;
+    on(eventType: StoreEventType | StoreEventType[], callback: (storeState: StoreState, prevState: StoreState, type: StoreEventType) => void): StoreEvent<StoreState>;
+}
---
+++ lib/StorePersistentDriver.d.ts
@@ -0,0 +1,26 @@
+export interface StorePersistentDump<StoreState> {
+    dumpHistory: StorePersistentPacket<StoreState>[];
+}
+export interface StorePersistentPacket<StoreState> {
+    data: StoreState;
+    timestamp: number;
+}
+export declare abstract class StorePersistentDriver<StoreState> {
+    readonly name: string;
+    readonly lifetime: number;
+    persistence: boolean;
+    constructor(name: string, lifetime?: number);
+    initialState: StoreState;
+    abstract type: string;
+    abstract write(pack: StorePersistentPacket<StoreState>): any;
+    abstract read(): StorePersistentPacket<StoreState>;
+    abstract saveDump(pack: StorePersistentPacket<StoreState>): number;
+    abstract readDump(id: number): StorePersistentPacket<StoreState>;
+    abstract resetHistory(): any;
+    abstract getDumpHistory(): number[];
+    abstract removeDump(timestamp: number): any;
+    pack(data: StoreState): StorePersistentPacket<StoreState>;
+    reset(): StorePersistentPacket<StoreState>;
+    get storeName(): string;
+    get dumpHistoryName(): string;
+}
