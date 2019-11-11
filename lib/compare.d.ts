e31b3001fa7bad32f8f824e2d6482b98d5ef3514
Ruslan Shashkov <ruslanchek@gmail.com>
2019-11-11 20:30:42 +0200

    .npmrc added

--- lib/compare.d.ts
+++
@@ -1,1 +0,0 @@
-export declare function areSimilar(a: any, b: any, ...exceptionList: Array<string>): boolean;
--- lib/followStore.d.ts
+++
@@ -1,3 +0,0 @@
-import * as React from 'react';
-import { Store } from './Store';
-export declare const followStore: <StoreState>(store: Store<StoreState>) => (WrappedComponent: React.ComponentClass<any, any>) => any;
