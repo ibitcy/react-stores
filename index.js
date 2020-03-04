7fae6b5e2fbd51a3d74c79a74ce14eb045713bcb
konstantin24121 <konstantin24121@gmail.com>
2020-03-04 15:08:08 +0200

    Fix link to dev package

--- index.js
+++ index.js
@@ -3,5 +3,5 @@
 if (process.env.NODE_ENV === 'production') {
   module.exports = require('./lib/index.js');
 } else {
-  module.exports = require('./dev/index.dev.js');
+  module.exports = require('./lib/index.dev.js');
 }
